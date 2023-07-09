11d443c4bb4d66e16da486918e76213d3648baa8
David Cho <gcho13@my.bcit.ca>
2023-07-09 00:04:13 -0400

    Finish implemented matrix functions

--- app/components/matrixCalculate/MatrixCalculator.js
+++ app/components/matrixCalculate/MatrixCalculator.js
@@ -7,20 +7,16 @@ import MatrixSelect from "./MatrixSelect";
 import CalculateMatrixResult from "./CalculateMatrixResult"; // import this component
 import { calculateMatrixOperation } from '../../utils/matrixOperations.js';
 
-export default function MatrixCalculator({
-  matrix1,
-  setMatrix1,
-  matrix2,
-  setMatrix2,
-  operation,
-  setOperation,
-}) {
+export default function MatrixCalculator() {
+  const [matrix1, setMatrix1] = useState("");
+  const [matrix2, setMatrix2] = useState("");
+  const [matrixOperation, setMatrixOperation] = useState("Add");
   const [result, setResult] = useState(""); // Add a new state variable for the result
 
   // Add a new function to handle the 'Calculate' button click
   const calculateResult = () => {
     try {
-      const result = calculateMatrixOperation(matrix1, matrix2, operation);
+      const result = calculateMatrixOperation(matrix1, matrix2, matrixOperation);
       setResult(result);
     } catch (err) {
       console.error(err);
@@ -31,7 +27,7 @@ export default function MatrixCalculator({
   return (
     <div>
       <MatrixInput matrix={matrix1} setMatrix={setMatrix1} id="matrix1" />
-      <MatrixSelect operation={operation} setOperation={setOperation} />
+      <MatrixSelect matrixOperation={matrixOperation} setMatrixOperation={setMatrixOperation} />
       <MatrixInput matrix={matrix2} setMatrix={setMatrix2} id="matrix2" />
       <button onClick={calculateResult}>
         Calculate
--- app/utils/matrixOperations.js
+++ app/utils/matrixOperations.js
@@ -1,57 +1,114 @@
 "use client";
 
 export function calculateMatrixOperation(matrix1, matrix2, operation) {
-    // Parse matrix strings into 2D arrays
-    const m1 = matrix1.split('\n').map(row => row.split(' ').map(Number));
-    const m2 = matrix2.split('\n').map(row => row.split(' ').map(Number));
-  
-    let result;
-  
-    switch(operation) {
-      case "Add":
-        result = addMatrices(m1, m2);
-        break;
-      case "Subtract":
-        result = subtractMatrices(m1, m2);
-        break;
-      case "Multiply":
-        result = multiplyMatrices(m1, m2);
-        break;
-      case "Transpose":
-        result = transposeMatrix(m1); // Let's suppose it transposes the first matrix
-        break;
-      case "Determinant":
-        result = determinantMatrix(m1); // Let's suppose it calculates the determinant of the first matrix
-        break;
-      case "Inverse":
-        result = inverseMatrix(m1); // Let's suppose it calculates the inverse of the first matrix
-        break;
-      default:
-        throw new Error(`Unsupported operation: ${operation}`);
-    }
-  
+  // Parse matrix strings into 2D arrays
+  const m1 = matrix1.split("\n").map((row) => row.split(" ").map(Number));
+  const m2 = matrix2.split("\n").map((row) => row.split(" ").map(Number));
+
+  let result;
+
+  switch (operation) {
+    case "Add":
+      result = addMatrices(m1, m2);
+      break;
+    case "Subtract":
+      result = subtractMatrices(m1, m2);
+      break;
+    case "Multiply":
+      result = multiplyMatrices(m1, m2);
+      break;
+    case "Transpose":
+      result = transposeMatrix(m1); // It transposes the first matrix
+      break;
+    case "Determinant":
+      result = determinantMatrix(m1); // It calculates the determinant of the first matrix
+      break;
+    case "Inverse":
+      result = inverseMatrix(m1); // It calculates the inverse of the first matrix
+      break;
+    default:
+      throw new Error(`Unsupported operation: ${operation}`);
+  }
+
+  if (Array.isArray(result)) {
     // Convert result back into a string
-    return result.map(row => row.join(' ')).join('\n');
+    return result.map((row) => row.join(" ")).join("\n");
+  } else {
+    // If result is not an array, convert it to a string and return it
+    return result.toString();
   }
-  
-  // Below, you would have to define your matrix operation functions such as addMatrices, subtractMatrices, etc.
-  // For example, the addMatrices function might look like this:
-  
-  function addMatrices(m1, m2) {
-    if (m1.length !== m2.length || m1[0].length !== m2[0].length) {
-      throw new Error('Matrices must have the same dimensions to be added');
+}
+
+function addMatrices(m1, m2) {
+  if (m1.length !== m2.length || m1[0].length !== m2[0].length) {
+    throw new Error("Matrices must have the same dimensions to be added");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < m1.length; i++) {
+    const row = [];
+    for (let j = 0; j < m1[i].length; j++) {
+      row.push(m1[i][j] + m2[i][j]);
     }
-  
-    const result = [];
-  
-    for (let i = 0; i < m1.length; i++) {
-      const row = [];
-      for (let j = 0; j < m1[i].length; j++) {
-        row.push(m1[i][j] + m2[i][j]);
+    result.push(row);
+  }
+
+  return result;
+}
+
+function subtractMatrices(m1, m2) {
+  if (m1.length !== m2.length || m1[0].length !== m2[0].length) {
+    throw new Error("Matrices must have the same dimensions to be subtracted");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < m1.length; i++) {
+    const row = [];
+    for (let j = 0; j < m1[i].length; j++) {
+      row.push(m1[i][j] - m2[i][j]);
+    }
+    result.push(row);
+  }
+
+  return result;
+}
+
+function multiplyMatrices(m1, m2) {
+  if (m1.length !== m2.length) {
+    throw new Error("The number of columns in the first matrix must be equal to the number of rows in the second matrix");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < m1.length; i++) {
+    const row = [];
+    for (let j = 0; j < m2[0].length; j++) {
+      let sum = 0;
+      for (let k = 0; k < m1[i].length; k++) {
+        sum += m1[i][k] * m2[k][j];
       }
-      result.push(row);
+      row.push(sum);
     }
-  
-    return result;
+    result.push(row);
   }
-  
+
+  return result;
+}
+
+function transposeMatrix(matrix) {
+  return matrix[0].map((_, i) => matrix.map((row) => row[i]));
+}
+
+// This implementation assumes that you've imported the mathjs library
+import * as math from 'mathjs';
+
+function determinantMatrix(matrix) {
+  return math.det(matrix);
+}
+
+function inverseMatrix(matrix) {
+  return math.inv(matrix);
+}
+
